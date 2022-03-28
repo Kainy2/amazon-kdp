9bf8deaabba34c40d37dd375820ef2ddc2c4c620
Kainy <iartkhay@gmail.com>
2022-03-28 13:25:58 +0100

    More illustrations and footer section

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface Props {
   children: any;
   className?: string; 
+  href?: string; 
 }
 const Button : React.FC<Props> = ({children, className, ...props}: Props) => {  return (
     <div className={` ${className} bg-orange-600 hover:bg-orange-500 active:bg-orange-400 transition-colors py-3 px-8 rounded w-max mx-auto m-2 text-gray-100 cursor-pointer`}>
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,27 +1,19 @@
-// import Button from "./Button";
+import Button from "./Button";
 
 
 interface Props {
   children?: any;
   className?: string
 }
-const Footer: React.FC<Props> = ({children, className, ...props}) => {
-
-    return (
-        <div>
-            <div className="flex w-full  mt-32 justify-evenly items-center font-header text-slate-200 py-10 bg-priBg">
-                <div className="text-xl">
-                    Book a call with us now
-                </div>
-                <div id='contact-us' className=''>
-                    <div className='w-max bg-gray-300 px-2'>
-                        {/* <Phone sx={{ color: 'black' }} />
-                        <Button href="tel:+2348148090531"> Call Now </Button> */}
-                    </div>
-                </div>
-            </div>
+const Footer: React.FC<Props> = ({children, className, ...props}) => (
+    <div className="flex w-full  mt-32 justify-evenly items-center font-header bg-zinc-700 text-orange-100 py-10 bg-priBg">
+        <div className="text-xl">
+            Book a call with us now
         </div>
-    )
-}
+        <div id='contact-us' className=''>
+            <Button className=" text-zinc-100" href="tel:+2348148090531"> Call Now </Button>
+        </div>
+    </div>
+)
 
 export default Footer
